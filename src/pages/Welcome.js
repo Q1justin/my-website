56d8f0eda4a5a37223716dea139cddee277658f3
Q1justin <q1justindo@gmail.com>
2022-09-07 21:30:06 -0400

    height change

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -2,8 +2,17 @@ import Snorlax from '../images/snorlax.png';
 
 function Welcome () {
     return (
-        <div>
-            <img className="welcome-image" src={Snorlax} alt="Snorlax-Munchlax" />
+        <div className="Welcome">
+            <div className="content">
+                <div className="introduction">
+                    <h1>Hello, I am Gyuwon Do</h1>
+                    <h3>안녕하세요, 도규원입니다</h3>
+                </div>
+                <div className="image-container">
+                    <img className="welcome-image" src={Snorlax} alt="Snorlax-Munchlax" />                
+                </div>
+            </div>
+           
         </div>
     )
 }
