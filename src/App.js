a1222a8424e2388e47792a66e6adc03f857482bd
Gyuwon (Justin) Do <q1justindo@gmail.com>
2023-04-10 20:45:00 -0400

    Updated link

--- src/App.js
+++ src/App.js
@@ -34,21 +34,42 @@ function App () {
                 </div>     
                 <div className="main-container">
                   <div className="about-me pure-u-1-2">
-                  <h2>Personal</h2>
-                    <li>My name is Gyuwon Do, but I like to go by Justin</li>
-                    <li>I am from South Korea, but grew up in Germany and the Czech Republic</li>
-                    <li>I came to the U.S. in 2015 to attend NYU</li>
-                    <li>I love to exercise and love the outdoors</li>
-                    <li>I am a <b>HUGE</b> cat person</li>
+                    <h2>Personal</h2>
+                        <li>My name is Gyuwon Do, but I like to go by Justin</li>
+                        <li>I am from South Korea, but grew up in Germany and the Czech Republic</li>
+                        <li>I came to the U.S. in 2015 to attend NYU</li>
+                        <li>I love to exercise and love the outdoors</li>
+                        <li>I am a <b>HUGE</b> cat person</li>
 
                     <h2>Professional</h2>
-                    <li>I am a frontend developer primarily using React and Javascript</li>
-                    <li>I have been in the tech industry for 3 years</li>
-                    <li>I get motivated by projects that are widely used by different people</li>
-                    <li>I want to work for a company that has a clear and righteous mission</li>
+                        <li>I am a frontend developer primarily using React and Javascript</li>
+                        <li>I have been in the tech industry for 3 years</li>
+                        <li>I get motivated by projects that are widely used by different people</li>
+                        <li>I want to work for a company that has a clear and righteous mission</li>
+                    <h2>Proficiencies</h2>
+                        <div className="proficiencies-container">
+                            <div className="pure-u-1-3">
+                                <ul>Javascript</ul>
+                                <ul>Python</ul>
+                                <ul>Java</ul>
+                                <ul>C</ul>
+                            </div>
+                            <div className="pure-u-1-3">
+                                <ul>React.js</ul>
+                                <ul>React Native</ul>
+                                <ul>Next.js</ul>
+                                <ul>Node.js</ul>
+                            </div>
+                            <div className="pure-u-1-3">
+                                <ul>PostgreSQL</ul>
+                                <ul>MySQL</ul>
+                                <ul>Postman</ul>
+                                <ul>Heroku</ul>
+                            </div>
+                        </div>
                   </div>
                   <div className="resume-container pure-u-1-2">
-                    <iframe src='https://drive.google.com/file/d/1ZR0E3bVy9lwaqlap6FW8LOI5RKLlla8Z/preview#zoom=20' className="resume" />
+                    <iframe src='https://drive.google.com/file/d/1yTkp77avWGa3BwQJQiSrqqH2W-hn4GQm//preview#zoom=20' className="resume" />
                   </div>                  
                 </div>   
             </div>
